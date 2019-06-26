072004948a2e2f5f21894a28f44bb968b6c2238a
mangotapioca13 <angela.liu1399@gmail.com>
2019-06-25 17:36:42 -0700

    first commit

---
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -0,0 +1,100 @@
+// Angela Liu
+// FBU 06252019
+
+package com.example.simpletodo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // read instead of create new Array since we want to pull up any old notes in the list
+        readItems();
+
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(itemsAdapter);
+
+        // mock data
+        // items.add("First Item");
+        // items.add("Second Item");
+
+        setupListViewListener();
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+        writeItems();
+        Toast.makeText(getApplicationContext(), "Item added to list", Toast.LENGTH_SHORT).show();
+    }
+
+    private void setupListViewListener() {
+        // information log
+        Log.i("MainActivity", "Setting up listener on list view");
+
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // information log generated whenever an item is removed
+                Log.i("MainActivity", "Item removed from list: " + position);
+
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+    }
+
+    private File getDataFile() {
+        return new File(getFilesDir(), "todo.txt");
+    }
+
+    private void readItems() {
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading file", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        try {
+            // writes file of all the lists
+            // method is called twice (removing and adding) since the list
+            // will be updated
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error writing file", e);
+        }
+    }
+}
